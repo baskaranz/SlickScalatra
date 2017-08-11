174256eb699c9b18c6ff4ea73ebfc6d767b4507b
Baskaran <you@example.com>
2017-08-11 11:45:58 +0800

    product dao comments

--- src/main/scala/com/tookitaki/dao/ProductsDAO.scala
+++ src/main/scala/com/tookitaki/dao/ProductsDAO.scala
@@ -8,6 +8,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object ProductsDAO {
 
+  /** Returns the list of all the products available.
+    *
+    * @return A future value with the list of products List[Pdt]
+    */
   def getAllProducts: Future[List[Pdt]] = Future {
     Products.all
   }
